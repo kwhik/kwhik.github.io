30df78cd35448d62ea1be368f2d75e17fee49fa4
Mark Fussell <mark.fussell@emenar.com>
2026-07-11 16:44:08 -0700

    Remove extraneous constants

--- src/processPosts.py
+++ src/processPosts.py
@@ -1,35 +1,20 @@
 #!/usr/bin/env python
 """\
-Process the topics.md file to find citations and hook them to the known sources.
+Process the files in 'sources_filepath' to create make H3 headers linkable and build a TOC that can be used by the file.
 
-Directly modifies the 'docs/topics.md' file although it creates a backup.  Could change this
-to using a '.local' version of those files when working with the script, or to take them in as a parameter.
-But given the directory is under 'git' management, wiping the file isn't particularly problematic.
-
-Also regenerates and rewrites the 'docs/_data/citations.yml' file, but that is a derived file and all content
-is sourced from the 'topics.md' file.
-
-It expects the file paths to be from the 'home' directory.  Also the 'sources' array is manually synchronized
-with the '_sources' directory.
-
-Usage: python src/processTopicCitations.py
+Usage: python src/processPosts.py
 """
 
 import fileinput
 import re
 import sys
-from urllib.parse import urlsplit
-import frontmatter
 import os
-import json
 
 # Current collection of sources: derived from '_sources' directory metadata information
 sources = {}
 
 # Filepaths relative to the 'home' directory of the project
 # Expecting the script to be called from there.
-topics_filepath = "./docs/topics.md"
-citation_filepath = "./docs/_data/citations.yml"
 sources_filepath = "./docs/_posts"
 includes_filepath = "./docs/_includes/posts"
 
